bdda0e470969bbb50dc93e326861d2bdce15db07
Jeremy Rummel <jfrummel@gmail.com>
2020-01-28 09:10:20 -0600

    Restaurant project completed

--- sg-restaurant/js/contactForm.js
+++ sg-restaurant/js/contactForm.js
@@ -1,4 +1,32 @@
+// Main function to validate form inputs
 function validateForm() {
-  console.log("Working");
+  const name = document.forms["contactForm"]["name"].value;
+  const email = document.forms["contactForm"]["email"].value;
+  const phoneNumber = document.forms["contactForm"]["phone"].value;
+
+  if (name === "") {
+    alert("Name is required.");
+    document.forms["contactForm"]["name"].focus();
+    return false;
+  }
+  if (email === "") {
+    alert("Email is required.");
+    document.forms["contactForm"]["email"].focus();
+    return false;
+  }
+  if (phoneNumber === "" || isNaN(phoneNumber)) {
+    alert("Phone number is required.");
+    document.forms["contactForm"]["phone"].focus();
+    return false;
+  }
+  alert("Contact information has been validated.");
+  clearForm();
   return false;
+}
+
+// Clears input fields
+function clearForm() {
+  document.forms["contactForm"]["name"].value = "";
+  document.forms["contactForm"]["email"].value = "";
+  document.forms["contactForm"]["phone"].value = "";
 }
